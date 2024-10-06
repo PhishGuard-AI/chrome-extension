2f938ab7ae358cd2b16e129bb4293b6843127eab
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2024-10-06 11:56:01 -0400

    add boiler plate code for the chrome extension

---
+++ content.js
@@ -0,0 +1,2 @@
+// Log the URL of the current tab
+console.log("PhishGuard AI is active on:", window.location.href);
---
+++ popup.js
@@ -0,0 +1,3 @@
+document.getElementById("scanButton").addEventListener("click", () => {
+  alert("Scanning URL...");
+});
